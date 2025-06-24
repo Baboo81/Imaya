80bcde74780af53e3fd82864b3b7abaecc582920
Baboo81 <magicbaboo@gmail.com>
2025-06-24 10:51:09 +0200

    Debug

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description"
-            content=" {{ $metaDesc }} ">
+            content=" {{ $meta['description'] ?? $metaDesc ?? '' }}">
         <meta name="keywords"
-            content=" {{ $metaKeyWords }} ">
+            content=" {{ $meta['keywords'] ?? $metaKeyWords ?? '' }}">
         <!-- Google Tag Manager Google analytics-->
         <script>(function (w, d, s, l, i) {
                 w[l] = w[l] || []; w[l].push({
@@ -47,5 +47,5 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
         <script src="{{ asset('assets/js/main.js') }}" defer></script>
         
-        <title>{{ $pageTitle }}</title>
+        <title>{{ $pageTitle ?? '' }}</title>
     
