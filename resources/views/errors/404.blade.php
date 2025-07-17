7ab6b3ce1698abe60c7d4d8921c5f7bd0ff9399c
Baboo81 <magicbaboo@gmail.com>
2025-07-17 11:51:17 +0200

    page : error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,9 @@
 
 @extends(('layouts.app'))
 
+@section('hideFooter', true)
+@section('hideNav', true)
+
 @section('styles')
     <link rel="stylesheet" href="{{ asset('assets/css/error.css') }}">
 @endsection
@@ -10,16 +13,16 @@
 @section('content')
 
 <div class="container text-center py-5">
-    <h1 class="display-1 text-danger">404</h1>
+    <h1 class="display-1 text-primary">404</h1>
     <h2 class="mb-4">Oups... Page introuvable</h2>
     <p class="lead mb-4">La page que vous cherchez n'existe pas ou a été déplacée.</p>
     
-    <a href="{{ url('/') }}" class="btn btn-order btn-lg mt-2 rounded-5">
+    <a href="{{ url('/') }}" class="btn btn-order btn-lg mt-2 rounded-5 DastinFont">
         Retour à l'accueil
     </a>
 
     <div class="mt-5">
-        <img src="{{ asset('assets/img/error/error404.svg') }}" alt="Erreur 404" class="img-fluid" style="max-width: 400px;">
+        <img src="{{ asset('assets/img/error/error404.svg') }}" alt="Erreur 404" class="img-fluid" style="max-width: 800px;">
     </div>
 </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,17 @@
         @yield('styles')
     </head>
     <body>
-        <header>
-            @include('partials.nav')
-        </header>
+        @if (empty(trim($__env->yieldContent('hideNav'))))
+            <header>
+                @include('partials.nav')
+            </header>
+        @endif
         <main>
             @yield('content')
         </main>
-        @include('partials.footer')
+
+       @if (empty(trim($__env->yieldContent('hideFooter'))))
+            @include('partials.footer')
+       @endif
     </body>
 </html>
