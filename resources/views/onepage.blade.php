ae61649114d7b5971e751ad1976f00f7d5688b14
Baboo81 <magicbaboo@gmail.com>
2025-08-20 11:25:35 +0200

    Debug

--- resources/views/onepage.blade.php
+++ resources/views/onepage.blade.php
@@ -316,89 +316,89 @@
                     </ul>
                 </article>
 
-{{-- Retraites --}}
-<article class="card-body my-5 blocRetraites">
-    <div class="my-5">
-        <h3 class="card-title whiteFont mb-5">
-            {{ $data['activite_subTitle2'] ?? '' }}
-        </h3>
-    </div>
-
-    <ul class="list-group list-group-flush rounded-2">
-        @foreach ($data['activites']['retraites'] as $retraite)
-            <li class="list-group-item text-center">
-                <div class="row justify-content-center mt-4 text-center">
-                    <h4 class="my-5">{{ $retraite['groupe'] }}</h4>
-
-                    <div class="retraiteImg w-100">
-                        {{-- Si plusieurs images => carousel, sinon image simple --}}
-                        @if (!empty($retraite['images']) && is_array($retraite['images']))
-                            @php $carouselId = 'carousel-' . $loop->index; @endphp
-                            <div id="{{ $carouselId }}" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    @foreach ($retraite['images'] as $index => $img)
-                                        <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                                            <img
-                                                src="{{ asset('assets/img/activités/retraites/' . ltrim($img, '/')) }}"
-                                                class="d-block w-100 img-half"
-                                                alt="Image retraite {{ $retraite['groupe'] }}"
-                                            >
-                                        </div>
-                                    @endforeach
-                                </div>
-
-                                @if (count($retraite['images']) > 1)
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#{{ $carouselId }}" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon"></span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#{{ $carouselId }}" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon"></span>
-                                    </button>
-                                @endif
-                            </div>
-
-                        @elseif (!empty($retraite['image']) && is_string($retraite['image']))
-                            <img
-                                src="{{ asset('assets/img/activités/retraites/' . ltrim($retraite['image'], '/')) }}"
-                                alt="Image retraite {{ $retraite['groupe'] }}"
-                                class="img-half"
-                            >
-                        @endif
+    {{-- Retraites --}}
+                <article class="card-body my-5 blocRetraites">
+                    <div class="my-5">
+                        <h3 class="card-title whiteFont mb-5">
+                            {{ $data['activite_subTitle2'] ?? '' }}
+                        </h3>
                     </div>
 
-                    <section class="d-flex flex-wrap justify-content-center">
-                        @foreach ($retraite['evenements'] as $event)
-                            @php
-                                $pdfPath = $event['path'];
-                                $pdfExists = file_exists(public_path($pdfPath));
-                                $pdfUrl = asset($pdfPath);
-                            @endphp
+                    <ul class="list-group list-group-flush rounded-2">
+                        @foreach ($data['activites']['retraites'] as $retraite)
+                            <li class="list-group-item text-center">
+                                <div class="row justify-content-center mt-4 text-center">
+                                    <h4 class="my-5">{{ $retraite['groupe'] }}</h4>
 
-                            <article class="col-12 col-sm-6 col-lg-3 my-5">
-                                <h5 class="mb-4">{{ $event['titre'] }}</h5>
-                                <div class="blocBtn">
-                                    @if ($pdfExists)
-                                        <a href="{{ $pdfUrl }}"
-                                           class="btn btn-order btn-lg me-5 DastinFont rounded-5"
-                                           download="{{ $event['nom_pdf'] }}">
-                                            {{ $data['btn_enSavoirPlus'] ?? '' }}
-                                        </a>
-                                    @else
-                                        <button class="btn btn-secondary disabled">PDF indisponible</button>
-                                    @endif
+                                    <div class="retraiteImg w-100">
+                                        {{-- Si plusieurs images => carousel, sinon image simple --}}
+                                        @if (!empty($retraite['images']) && is_array($retraite['images']))
+                                            @php $carouselId = 'carousel-' . $loop->index; @endphp
+                                            <div id="{{ $carouselId }}" class="carousel slide" data-bs-ride="carousel">
+                                                <div class="carousel-inner">
+                                                    @foreach ($retraite['images'] as $index => $img)
+                                                        <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+                                                            <img
+                                                                src="{{ asset('assets/img/activités/retraites/' . ltrim($img, '/')) }}"
+                                                                class="d-block w-100 img-half"
+                                                                alt="Image retraite {{ $retraite['groupe'] }}"
+                                                            >
+                                                        </div>
+                                                    @endforeach
+                                                </div>
+
+                                                @if (count($retraite['images']) > 1)
+                                                    <button class="carousel-control-prev" type="button" data-bs-target="#{{ $carouselId }}" data-bs-slide="prev">
+                                                        <span class="carousel-control-prev-icon"></span>
+                                                    </button>
+                                                    <button class="carousel-control-next" type="button" data-bs-target="#{{ $carouselId }}" data-bs-slide="next">
+                                                        <span class="carousel-control-next-icon"></span>
+                                                    </button>
+                                                @endif
+                                            </div>
+
+                                        @elseif (!empty($retraite['image']) && is_string($retraite['image']))
+                                            <img
+                                                src="{{ asset('assets/img/activités/retraites/' . ltrim($retraite['image'], '/')) }}"
+                                                alt="Image retraite {{ $retraite['groupe'] }}"
+                                                class="img-half"
+                                            >
+                                        @endif
+                                    </div>
+
+                                    <section class="d-flex flex-wrap justify-content-center">
+                                        @foreach ($retraite['evenements'] as $event)
+                                            @php
+                                                $pdfPath = $event['path'];
+                                                $pdfExists = file_exists(public_path($pdfPath));
+                                                $pdfUrl = asset($pdfPath);
+                                            @endphp
+
+                                            <article class="col-12 col-sm-6 col-lg-3 my-5">
+                                                <h5 class="mb-4">{{ $event['titre'] }}</h5>
+                                                <div class="blocBtn">
+                                                    @if ($pdfExists)
+                                                        <a href="{{ $pdfUrl }}"
+                                                        class="btn btn-order btn-lg me-5 DastinFont rounded-5"
+                                                        download="{{ $event['nom_pdf'] }}">
+                                                            {{ $data['btn_enSavoirPlus'] ?? '' }}
+                                                        </a>
+                                                    @else
+                                                        <button class="btn btn-secondary disabled">PDF indisponible</button>
+                                                    @endif
+                                                </div>
+                                            </article>
+                                        @endforeach
+                                    </section>
                                 </div>
-                            </article>
+                            </li>
                         @endforeach
-                    </section>
-                </div>
-            </li>
-        @endforeach
-    </ul>
-</article>
-</div>
-</div>
-</section>
-{{-- Section : Activités END--}}
+                    </ul>
+                </article>
+            </div>
+        </div>
+    </section>
+    {{-- Section : Activités END--}}
 
     {{-- Anchor : Section Casa Imayah --}}
     <a id="CasaImayah" href=""></a>
@@ -567,3 +567,5 @@
     </section>
     {{-- Section : Contact END --}}
 @endsection
+
+
