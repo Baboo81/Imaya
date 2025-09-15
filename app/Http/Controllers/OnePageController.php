2569d09aa7d479a3bb8722d361c559075b044370
Baboo81 <magicbaboo@gmail.com>
2025-09-15 16:35:19 +0200

    Cleaning

--- app/Http/Controllers/OnePageController.php
+++ app/Http/Controllers/OnePageController.php
@@ -11,14 +11,13 @@ class OnePageController extends Controller
 
         //Inclure les data
         $data = include resource_path('data/onepageData.php');
-       //$creations = include resource_path('data/onepageData.php');
 
         return view('onepage' , [
-            'pageTitle' => 'Imayah (France-Alexandra Vigouroux)',
-            'metaDesc' => 'Découvrez Imayah le site de France-Alexandra Vigouroux est un centre de soins énergétiques situé en Sicile et à Bruxelles. France-Alexandra Vigouroux propose des retraites, des ateliers créatifs ainsi que des soins énergétiques, des massages vibratoires, un accompagnement thérapeutique, des méditations, un soin de l\'âme, des bains sonore. Il est possible aussi d\'acheter ses créations réalisées dans un état de transe.',
-            'metaKeyWords' => 'France-Alexandra Vigouroux, Centre de soins énergétiques, retraites, ateliers créatifs, massages vibratoires, accompagnement thérapeutique, méditations, soin de l\'âme, bain sonore.',
-            'resetCss' => 'assets/css/reset.css',
-            'customCss' => 'assets/css/onePage.css',
+            'pageTitle' => "Imayah (France-Alexandra Vigouroux)",
+            'metaDesc' => "Découvrez Imayah le site de France-Alexandra Vigouroux est un centre de soins énergétiques situé en Sicile et à Bruxelles. France-Alexandra Vigouroux propose des retraites, des ateliers créatifs ainsi que des soins énergétiques, des massages vibratoires, un accompagnement thérapeutique, des méditations, un soin de l'âme, des bains sonore. Il est possible aussi d'acheter ses créations réalisées dans un état de transe.",
+            'metaKeyWords' => "France-Alexandra Vigouroux, Centre de soins énergétiques, retraites, ateliers créatifs, massages vibratoires, accompagnement thérapeutique, méditations, soin de l'âme, bain sonore.",
+            'resetCss' => "assets/css/reset.css",
+            'customCss' => "assets/css/onePage.css",
             'data' => $data,
             'creations' => $data['creations'] ?? [],
         ]);
